83b16659448ab883f9c627e8ae0803621aa2f00f
LaChope <sofiacarretonprieto@gmail.com>
2023-07-28 10:24:00 +0200

    .

--- ZOO/js/script.js
+++ ZOO/js/script.js
@@ -1,3 +1,7 @@
+
+
+//--------COMPRAR ENTRADAS--------
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -18,4 +22,29 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
-  
+
+
+//---------BANER QUE CAMBIA LA IMAGEN----------------
+
+let arrayImg=new Array(4)  //-declarar unidades del array-
+arrayImg[0]=new Image();
+arrayImg[0].src="img/jimmi-el-mono.webp";
+arrayImg[1]=new Image();
+arrayImg[1].src="img/cabarceno.webp";
+arrayImg[2]=new Image();
+arrayImg[2].src="img/tucan.webp";
+arrayImg[3]=new Image();
+arrayImg[3].src="img/mapache.webp";
+contador=0
+
+function cambiarImagen(){
+  document.querySelector("#fotosBaner").src=arrayImg[contador].src;
+  contador++;
+  //operador ternario
+  contador=contador==4?contador=0:contador=contador;
+  setTimeout("cambiarImagen()", 5000);//se llama a sí mismo  
+}
+
+window.addEventListener("load", ()=>{
+  cambiarImagen();
+})
---
+++ ZOO/js/scriptContacto.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function(){
+    let seleccion=document.querySelector("#formcontacto");
+    //declarar div ocultos
+    let divTel=document.querySelector(".teleOculto");
+    let divEmail=document.querySelector(".emailOculto");
+    let divCorreo=document.querySelector(".cpOculto");
+    seleccion.addEventListener("change", ()=>{
+        let valorSeleccion=seleccion.value;
+        switch (valorSeleccion) {
+            case "telefono":{
+                divTel.style.display="block";
+                divEmail.style.display="none";
+                divCorreo.style.display="none";
+                break;
+            }               
+            case "email":{
+                divEmail.style.display="block";
+                divTel.style.display="none";
+                divCorreo.style.display="none";
+                break;
+            }
+            case "correo":{
+                divCorreo.style.display="block";
+                divTel.style.display="none";
+                divEmail.style.display="none";
+                break;
+            }
+        }
+    })
+
+    let boton=document.querySelector(".button-3");
+    boton.addEventListener("click",()=>{
+        let nombre=document.querySelector("#nombre").value;
+        let apellidos=document.querySelector("#lname").value;
+        let mns=document.querySelector("#mensaje").value;
+        console.log(nombre+" "+apellidos+" "+mns);
+    })
+})
